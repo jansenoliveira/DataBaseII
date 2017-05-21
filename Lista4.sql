722a9c3aec25969165fe81dde4bad1167eb2087c
Mauro de Carvalho <mauro.oak@outlook.com>
2017-05-21 15:24:41 -0300

    resolução exercicio 3 da lista 4

--- Lista4.sql
+++ Lista4.sql
@@ -28,3 +28,49 @@ RETURN (
 )
 
 SELECT * FROM SalesMonth(10,2001)
+
+
+/*Questao 3*/
+CREATE FUNCTION CategorySalesMonth (@month int, @year int)
+RETURNS TABLE
+AS
+RETURN (
+	SELECT
+		Production.ProductCategory.ProductCategoryID,
+		MONTH(Sales.SalesOrderHeader.OrderDate) as 'Mes',
+		YEAR(Sales.SalesOrderHeader.OrderDate) as 'Ano',
+		SUM(Sales.SalesOrderDetail.OrderQty) as 'Quantidade Total Vendida',
+		AVG(Sales.SalesOrderDetail.UnitPrice) as 'Preco Unitario Médio',
+		SUM(Sales.SalesOrderDetail.UnitPriceDiscount) as 'Desconto Total',
+		SUM(Sales.SalesOrderDetail.LineTotal) as 'Valor Total Vendido'
+	FROM
+		Sales.SalesOrderDetail
+	INNER JOIN
+		Sales.SalesOrderHeader
+	ON
+		Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID
+	INNER JOIN
+		Production.Product
+	ON
+		Production.Product.ProductID = 	Sales.SalesOrderDetail.ProductID
+	INNER JOIN
+		Production.ProductSubcategory
+	ON	
+		Production.ProductSubcategory.ProductSubcategoryID = Production.Product.ProductSubcategoryID
+	INNER JOIN 
+		Production.ProductCategory
+	ON
+		Production.ProductCategory.ProductCategoryID = Production.ProductSubcategory.ProductCategoryID
+	WHERE 
+		MONTH(Sales.SalesOrderHeader.OrderDate) = @month
+			AND
+		YEAR(Sales.SalesOrderHeader.OrderDate) = @year
+	GROUP BY
+			Production.ProductCategory.ProductCategoryID,
+			MONTH(Sales.SalesOrderHeader.OrderDate),
+			YEAR(Sales.SalesOrderHeader.OrderDate)
+)
+
+SELECT * FROM CategorySalesMonth(10,2001)
+
+/*Questao 4*/
